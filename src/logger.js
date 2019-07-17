820cf0279c0be26469eab6144e929a1232f4ad9e
Batista Harahap <batista@bango29.com>
2019-07-17 15:53:00 +0700

    changed logger

--- src/logger.js
+++ src/logger.js
@@ -1,32 +1,12 @@
-import * as winston from "winston";
+const log = (message) => {
+  const ts = new Date().toISOString()
+  console.log(`${ts} [info]: ${message}`)
+}
 
-const defaultFormat = winston.format.printf((info) => {
-  return `${info.timestamp} [${info.level}]: ${info.message}`;
-});
-
-const prettyJson = winston.format.printf(info => {
-  if (info.message.constructor === Object) {
-    info.message = JSON.stringify(info.message, null, 4)
-  }
-  return `${info.timestamp} [${info.level}]: ${info.message}`;
-})
-
-const createDevLogger = () => {
-  return winston.createLogger({
-    level: "silly",
-    transports: [
-      new winston.transports.Console({
-        format: winston.format.combine(
-          winston.format.timestamp(),
-          winston.format.colorize(),
-          defaultFormat,
-          prettyJson,
-        ),
-      }),
-    ],
-  });
-};
-
-const logger = createDevLogger()
+const logger = {
+  info: log,
+  error: log,
+  debug: log,
+}
 
 export default logger
